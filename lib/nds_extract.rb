09b6e340651223eb79a010d05da6367eeb05af33
Ruth Obe <ruthobey@gmail.com>
2020-02-18 21:38:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,7 +19,50 @@ def directors_totals(nds)
   
   
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
   result = {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
   }
   
